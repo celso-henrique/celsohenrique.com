b5924e98029cc5e5d80a1ba28f7cddeeb061d8fb
Celso Henrique <celso.henrique@hotmail.com.br>
2020-07-26 17:54:56 -0300

    Adding disqus

--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, Heading } from "theme-ui";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { DiscussionEmbed } from "disqus-react";
 import React from "react";
 import Layout from "./layout";
 import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags";
@@ -34,30 +35,38 @@ type PostProps = {
 const px = [`32px`, `16px`, `8px`, `4px`];
 const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`);
 
-const Post = ({ data: { post } }: PostProps) => (
-  <Layout>
-    <SEO
-      title={post.title}
-      description={post.description ? post.description : post.excerpt}
-      image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
-      pathname={post.slug}
-    />
-    <Heading variant="styles.h2">{post.title}</Heading>
-    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
-      <time>{post.date}</time>
-      {post.tags && (
-        <React.Fragment>
-          {` — `}
-          <ItemTags tags={post.tags} />
-        </React.Fragment>
-      )}
-      {post.timeToRead && ` — `}
-      {post.timeToRead && <span>{post.timeToRead} min de leitura</span>}
-    </p>
-    <section sx={{ my: 5, ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) } }}>
-      <MDXRenderer>{post.body}</MDXRenderer>
-    </section>
-  </Layout>
-);
+const Post = ({ data: { post } }: PostProps) => {
+  const disqusConfig = {
+    shortname: process.env.GATSBY_DISQUS_NAME,
+    config: { identifier: post.title }
+  }
+
+  return (
+    <Layout>
+      <SEO
+        title={post.title}
+        description={post.description ? post.description : post.excerpt}
+        image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
+        pathname={post.slug}
+      />
+      <Heading variant="styles.h2">{post.title}</Heading>
+      <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+        <time>{post.date}</time>
+        {post.tags && (
+          <React.Fragment>
+            {` — `}
+            <ItemTags tags={post.tags} />
+          </React.Fragment>
+        )}
+        {post.timeToRead && ` — `}
+        {post.timeToRead && <span>{post.timeToRead} min de leitura</span>}
+      </p>
+      <section sx={{ my: 5, ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) } }}>
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </section>
+      <DiscussionEmbed {...disqusConfig} />
+    </Layout>
+  );
+}
 
 export default Post;
