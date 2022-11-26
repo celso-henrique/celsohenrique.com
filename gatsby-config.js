0fcbe6d161611ec943a30b89c75113469ca4d45c
Celso Henrique <celso.henrique@hotmail.com.br>
2022-11-26 18:39:07 -0300

    Updating links, about me and hero text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,84 +1,80 @@
 require(`dotenv`).config({
-  path: `.env`
+    path: `.env`
 });
 
 module.exports = {
-  siteMetadata: {
-    siteTitle: `Celso Henrique`,
-    siteImage: `/banner.jpg`,
-    siteTitleAlt: `Celso Henrique`,
-    siteHeadline: `Celso Henrique`,
-    siteDescription: `Boas-vindas ao meu blog. Sou Engineering Manager na Creditas e aqui escrevo sobre tecnologia, programação, cultura, a web, meu cotidiano e mais.`,
-    siteUrl: `https://celsohenrique.com`,
-    siteLanguage: `pt-br`,
-    author: `@celsohenrique_`
-  },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`
-          },
-          {
-            title: `Sobre`,
-            slug: `/sobre`
-          }
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/celsohenrique_`
-          },
-          {
-            name: `Linkedin`,
-            url: `https://www.linkedin.com/in/devfrontend/`
-          },
-          {
-            name: `GitHub`,
-            url: `https://github.com/celso-henrique`
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/celsohenrq/`
-          }
-        ]
-      }
+    siteMetadata: {
+        siteTitle: `Celso Henrique`,
+        siteImage: `/banner.jpg`,
+        siteTitleAlt: `Celso Henrique`,
+        siteHeadline: `Celso Henrique`,
+        siteDescription: `Boas-vindas ao meu blog. Sou Engineering Manager na Creditas e aqui escrevo sobre tecnologia, programação, cultura, a web, meu cotidiano e mais.`,
+        siteUrl: `https://celsohenrique.com`,
+        siteLanguage: `pt-br`,
+        author: `@celsohenrique_`
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID
-      }
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Celso Henrique`,
-        short_name: `Celso Henrique`,
-        description: `Boas-vindas ao meu blog. Sou Engineering Manager na Creditas e aqui escrevo sobre tecnologia, programação, cultura, a web, meu cotidiano e mais.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#2d3748`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`
-          }
-        ]
-      }
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`
-  ]
+    plugins: [
+        {
+            resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+            options: {
+                navigation: [
+                    {
+                        title: `Blog`,
+                        slug: `/blog`
+                    },
+                    {
+                        title: `Sobre`,
+                        slug: `/sobre`
+                    }
+                ],
+                externalLinks: [
+                    {
+                        name: `Linkedin`,
+                        url: `https://www.linkedin.com/in/devfrontend/`
+                    },
+                    {
+                        name: `GitHub`,
+                        url: `https://github.com/celso-henrique`
+                    },
+                    {
+                        name: `E-mail`,
+                        url: `mailto:contato@celsohenrique.com`
+                    }
+                ]
+            }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_ANALYTICS_ID
+            }
+        },
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Celso Henrique`,
+                short_name: `Celso Henrique`,
+                description: `Boas-vindas ao meu blog. Sou Engineering Manager na Creditas e aqui escrevo sobre tecnologia, programação, cultura, a web, meu cotidiano e mais.`,
+                start_url: `/`,
+                background_color: `#fff`,
+                theme_color: `#2d3748`,
+                display: `standalone`,
+                icons: [
+                    {
+                        src: `/android-chrome-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/android-chrome-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`
+                    }
+                ]
+            }
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-netlify`
+    ]
 };
